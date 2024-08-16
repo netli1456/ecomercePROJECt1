237e9a7b6f9a1b534d34a4cfb81ae2ae17179f31
weezy1456 <w800548@gmail.com>
2024-08-16 12:49:12 +0100

    now cors

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    credentials: true,
-    origin: '*',
+    origin: 'https://aloservices-ji34.onrender.com', 
+    credentials: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200, 
   })
 );
 app.use(cookieParser());
